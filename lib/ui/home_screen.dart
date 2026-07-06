c89223ac4b391a5f350002302a9c285d9be26e77
Suprasidh <suprasidh.das@gmail.com>
2026-07-06 19:35:07 +0530

    feat: app shell, home, transactions, manual entry (cashew-style m3 ui)

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../data/db.dart';
 import '../services/notif_service.dart' show formatPaise;
 import '../state/providers.dart';
 import 'theme.dart';
